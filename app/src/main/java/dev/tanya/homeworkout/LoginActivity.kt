207c891112e6655338ddda1933c22f861c673cdb
tanyasismayiana <tanyamayiana@gmail.com>
2022-05-24 20:04:37 +0300

    First commit

--- app/src/main/java/dev/tanya/homeworkout/LoginActivity.kt
+++ app/src/main/java/dev/tanya/homeworkout/LoginActivity.kt
@@ -1,11 +1,13 @@
 package dev.tanya.homeworkout
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        setContentView(R.layout.activity_login)
     }
 }
