303420d0cd0a6920135ebbb83fc3e4ddfef1711d
tanyasismayiana <tanyamayiana@gmail.com>
2022-06-02 08:02:41 +0300

    Signup

--- app/src/main/java/dev/tanya/homeworkout/LoginActivity.kt
+++ app/src/main/java/dev/tanya/homeworkout/LoginActivity.kt
@@ -4,18 +4,19 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 
 class LoginActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        lateinit var etEmail:TextInputEditText
-        lateinit var etPassword:TextInputEditText
+        lateinit var etEmail: TextInputEditText
+        lateinit var etPassword: TextInputEditText
         lateinit var tilEmail:TextInputLayout
         lateinit var tilPassword:TextInputLayout
         lateinit var btnLogin:Button
         lateinit var tvSignUp:TextView
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         etEmail=findViewById(R.id.etEmail)
@@ -31,20 +32,25 @@ class LoginActivity : AppCompatActivity() {
         }
         btnLogin.setOnClickListener {
             validateLogin()
+
         }
     }
-
-
-    }
     fun validateLogin(){
-        var email = etEmail.text.toString()
-        var password= etPassword.text.toString()
+        var email =etEmail.text.toString()
+        var password=etPassword.text.toString()
 
         if (email.isBlank()) {
             tilEmail.error = "@string/errorEmail"
         }
+        if (password.isBlank()){
+            tilPassword.error="password is required"
+        }
+
 
     }
+
+    }
+
 
 
 
